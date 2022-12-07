efb90b2f1342480acb4d8fbbe60e2b4fd1dfe840
fivesun1228@163.com <fivesun1228@163.com>
2022-12-07 16:14:20 +0800

    build

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,6 +1,9 @@
 // 节流函数，多次重复操作一段时间只执行一次
 
-export const throttle = (fn: Function, time: number) => {
+export const throttle = <T extends (...args: any) => any>(
+    fn: T,
+    time: number
+) => {
     let timer: number | undefined = undefined;
     return (...args: any[]) => {
         if (timer) {
